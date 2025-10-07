c8a72d1ef7ed6ac64622d2b742f63f87caabae9f
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-10-07 22:25:04 +0200

    update

--- Home/views.py
+++ Home/views.py
@@ -3,6 +3,7 @@ from News.models import *
 from News.views import news_view
 from Team.models import *
 from Team.views import team_view
+from Appointment.forms import RandevuForm
 
 # Create your views here.
 
@@ -12,10 +13,12 @@ def index_view(request):
     category = Category.objects.all()
     author = Author.objects.all()
     team = Team.objects.all()
+    form = RandevuForm()
     
     return render(request, 'Home/index.html', {
         'news': news,
         'category':category,
         'author': author,
         'team':team,
+        'form': form,
     })
